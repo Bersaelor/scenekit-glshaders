925a0978da6a93087ec1d040eae418977430f452
Konrad Feiler <K_W_F@web.de>
2019-03-24 16:06:44 +0100

    fix sm_light

--- CustomShaders/sm_light.glsl
+++ CustomShaders/sm_light.glsl
@@ -1,4 +1,4 @@
-const float lightIntensity = 1.0;
+uniform float lightIntensity = 1.0;
 
 float diff = max(0.0, dot(_surface.normal, _light.direction));
 _lightingContribution.diffuse = 0.1 * (diff * _light.intensity.rgb);
@@ -9,4 +9,3 @@ _lightingContribution.diffuse += (back * _light.intensity.rgb);
 // rim/fresnel
 float fn = lightIntensity * 1.5 * pow(1.0 - dot(_surface.normal, _surface.view), 1.3);
 _lightingContribution.diffuse += (fn * _light.intensity.rgb);
-
