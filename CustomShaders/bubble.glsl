274275e556f2c452abe3ad4205a125f897e06c0b
Konrad Feiler <K_W_F@web.de>
2019-03-24 17:11:32 +0100

    fix bubble glsl

--- CustomShaders/bubble.glsl
+++ CustomShaders/bubble.glsl
@@ -1,4 +1,4 @@
-const float normalScaling = 1.5;
-const float timeScale = 3.0;
+uniform float normalScaling = 1.5;
+uniform float timeScale = 3.0;
 
 _geometry.position += vec4(_geometry.normal, 0.0) * sin(u_time * timeScale) * normalScaling;
