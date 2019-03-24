21b1c26c0a6da10edf94c85e89183a49bf4a18d7
Konrad Feiler <K_W_F@web.de>
2019-03-24 17:02:16 +0100

    make outline color a vec4 uniform

--- CustomShaders/outline.glsl
+++ CustomShaders/outline.glsl
@@ -1,9 +1,9 @@
-float PIover2 = (3.14159265358979 / 2.0);
+uniform vec4 outlineColor = vec4(0.0, 0.0, 0.0, 1.0);
 uniform float lineTolerance = 0.4;
 
 float dotProduct = dot(_surface.view, _surface.normal);
 
 if ( lineTolerance > dotProduct && dotProduct >  - lineTolerance ) {
-  _output.color.rgba = vec4(0.0, 0.0, 0.0, 1.0);
+  _output.color.rgba = outlineColor;
 }
 
