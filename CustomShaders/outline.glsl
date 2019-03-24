d2cfdf564bb75b3e638680fd9776c29625a83b20
Konrad Feiler <K_W_F@web.de>
2019-03-24 16:51:24 +0100

    outline has to be 90 degree rotated

--- CustomShaders/outline.glsl
+++ CustomShaders/outline.glsl
@@ -1,11 +1,9 @@
-// TODO invert so it does outline? Might have to use a geometry shader for this
-
 float PIover2 = (3.14159265358979 / 2.0);
-uniform float lineTolerance = 0.9;
+uniform float lineTolerance = 0.4;
 
 float dotProduct = dot(_surface.view, _surface.normal);
 
-if ( (PIover2 + lineTolerance) > dotProduct && dotProduct > (PIover2 - lineTolerance) ) {
+if ( lineTolerance > dotProduct && dotProduct >  - lineTolerance ) {
   _output.color.rgba = vec4(0.0, 0.0, 0.0, 1.0);
 }
 
